1e8aabd497562031a1bc1e509c980d14299acb27
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-05-28 09:14:27 +0300

    enable email verification

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,46 +1,25 @@
-<x-layouts.app>
-  {{-- <x-layouts.auth> --}}
-  {{-- <x-auth-card> --}}
-      {{-- <x-slot name="logo">
-          <a href="/">
-              <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-          </a>
-      </x-slot> --}}
+<x-layouts.auth>
+  <div class="mb-4 text-sm text-gray-600">
+    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+  </div>
 
-      <div class="mb-4 text-sm text-gray-600">
-          {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-      </div>
+  <form method="POST" action="{{ route('password.email') }}">
+    @csrf
 
-      <!-- Session Status -->
-      <x-auth-session-status class="mb-4" :status="session('status')" />
+    <!-- Email Address -->
+    <div>
+      <label for="email">{{ __('Email') }}</label>
 
-      <!-- Validation Errors -->
-      <x-auth-validation-errors class="mb-4" :errors="$errors" />
-      <section class="section is-large">
-        <div class="container">
-          <div class="columns is-centered">
-            <div class="column is-4">
-              <div class="card" >
-                <p class="title is-4 p-3">Login</p>
-                <div class="card-content">
-                  <div class="content">
+      <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+      @error('email')
+      {{ $message }}
+      @enderror
+    </div>
 
-      <form method="POST" action="{{ route('password.email') }}">
-          @csrf
-
-          <!-- Email Address -->
-          <div>
-              <x-label for="email" :value="__('Email')" />
-
-              <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-          </div>
-
-          <div class="flex items-center justify-end mt-4">
-              <x-button>
-                  {{ __('Email Password Reset Link') }}
-              </x-button>
-          </div>
-      </form>
-                  </div></div></div></div></div></div></section>
-  {{-- </x-auth-card> --}}
-</x-layouts.app>
+    <div class="flex items-center justify-end mt-4">
+      <button>
+        {{ __('Email Password Reset Link') }}
+      </button>
+    </div>
+  </form>
+</x-layouts.auth>
